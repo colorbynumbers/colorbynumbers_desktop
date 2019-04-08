b246e9531949a9c940b91b5f85c94f97ee63a6ed
SpurNut <Lio_Ko@gmx.de>
2019-04-08 13:55:10 +0200

    added gui without logic

--- src/colorbynumbers/ColorByNumber.py
+++ src/colorbynumbers/ColorByNumber.py
@@ -1,8 +1,18 @@
 # Created by Lionel Kornberger at 2019-04-01
 
+import sys
+from PySide2.QtWidgets import QApplication
+
 from model.Canvas import Canvas
+from view.MainWindow import MainWindow
 
 # ToDo create GUI and Controller and Model
 
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
 can = Canvas(64, 32)
 print(can)
+
+sys.exit(app.exec_())
--- src/colorbynumbers/view/MainWindow.py
+++ src/colorbynumbers/view/MainWindow.py
@@ -1,19 +1,12 @@
-# Created by Lionel Kornberger at 2019-04-06
+# Created by Lionel Kornberger at 2019-04-07
 
-import sys
-from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
-from PySide2.QtCore import QFile
+from PySide2.QtWidgets import QMainWindow
+from ui_mainwindow import Ui_MainWindow
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
 
-    ui_file = QFile("mainwindow.ui")
-    ui_file.open(QFile.ReadOnly)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    window.show()
-
-    sys.exit(app.exec_())
