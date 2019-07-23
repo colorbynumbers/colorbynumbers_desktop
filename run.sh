81432ef1573caeca303053751c554bdd65902fca
SpurNut <Lio_Ko@protonmail.com>
2019-07-23 23:47:25 +0200

    Added automatic installation of venv to shell script

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-source activate cbn
+if conda env list | grep -q 'cbn'; then
+   source activate cbn
+else
+   conda env create -f environment.yml
+   source activate cbn 
+fi
+
 cd src/
 python ColorByNumber.py
